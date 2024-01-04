3c12cd2dfd934db23b59eb281f65e80d0d6e1e5a
morning-firebird <victorogniewicz@gmail.com>
2024-01-05 02:51:34 +0300

    added preference support, fixed  language switching, refactored  some css and js, and localized the page

--- scripts/langselect.js
+++ scripts/langselect.js
@@ -1,14 +1,17 @@
 loadLang();
 
-function loadLang () {
-    const lang = navigator.language ? navigator.language : "en";
+function loadLang() {
+    let lang = localStorage.getItem('lang');
+    if (!lang) {
+        lang = navigator.language ? navigator.language : "en";
+    }
     const isRu = lang.includes("ru");
-    if (isRu)
-    {
+    if (isRu) {
+        localStorage.setItem('lang', "ru");
         window.location.href = "./mainrus.html";
     }
-    else
-    {
+    else {
+        localStorage.setItem('lang', "en");
         window.location.href = "./maineng.html";
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 window.onload = function () {
+    saveLang();
     initCopyrightYear();
     initAge();
+    initH1Animation();
     initTheme();
-    const sectSel = document.getElementById('section-selector');
-    sectSel.addEventListener('change', loadSection);
-    window.addEventListener('scroll', animateH);
+    initNavSelector();
 }
 
 function initCopyrightYear() {
@@ -23,16 +23,33 @@ function initAge() {
     ageLine.innerHTML = myAge;
 }
 
+function initH1Animation() {
+    window.addEventListener('scroll', animateH);
+}
+
 function initTheme() {
-    const lightIcon = document.getElementById('light-theme-icon');
-    const darkIcon = document.getElementById('dark-theme-icon');
+    const lightIcon = document.getElementById('unsel-light-theme-icon');
+    const darkIcon = document.getElementById('unsel-dark-theme-icon');
     lightIcon.addEventListener('click', setLightTheme);
     darkIcon.addEventListener('click', setDarkTheme);
 
-    const isPrefDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const theme = (isPrefDark === true) ? "dark" : "light";
+    let theme = localStorage.getItem('theme');
+    if (!theme) {
+        const isPrefDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        theme = (isPrefDark === true) ? "dark" : "light";
+    }
     setTheme(theme);
     loadColorTheme(theme);
+}
+
+function saveLang() {
+    console.log(document.documentElement.lang);
+    localStorage.setItem('lang', document.documentElement.lang);
+}
+
+function initNavSelector() {
+    const sectSel = document.getElementById('section-selector');
+    sectSel.addEventListener('change', loadSection);
 }
 
 function setLightTheme() { setTheme("light") };
@@ -40,30 +57,27 @@ function setDarkTheme() { setTheme("dark") };
 
 function setTheme(theme) {
     document.documentElement.className = theme;
-    localStorage.setItem("theme", theme);
+    localStorage.setItem('theme', theme);
     loadColorTheme(theme);
 }
 
 function loadColorTheme(theme) {
-    const lightIcon = document.getElementById('light-theme-icon');
-    const darkIcon = document.getElementById('dark-theme-icon');
+    const selLightIcon = document.getElementById('sel-light-theme-icon');
+    const unselLightIcon = document.getElementById('unsel-light-theme-icon');
+    const selDarkIcon = document.getElementById('sel-dark-theme-icon');
+    const unselDarkIcon = document.getElementById('unsel-dark-theme-icon');
 
     if (theme === "dark") {
-        lightIcon.setAttribute('src', "icons/sun-unselected.svg");
-        lightIcon.setAttribute('title', "Set to light theme");
-        lightIcon.setAttribute('alt', "Unfilled sun icon");
-        darkIcon.setAttribute('src', "icons/moon-selected.svg");
-        darkIcon.setAttribute('title', "Dark theme is set");
-        darkIcon.setAttribute('alt', "Filled moon icon");
-        darkIcon.edi
+        selLightIcon.style.display = 'none';
+        unselLightIcon.style.display = 'inline-block';
+        selDarkIcon.style.display = 'inline-block';
+        unselDarkIcon.style.display = 'none';
     }
     else {
-        lightIcon.setAttribute('src', "icons/sun-selected.svg");
-        lightIcon.setAttribute('title', "Light theme is set");
-        lightIcon.setAttribute('alt', "Filled sun icon");
-        darkIcon.setAttribute('src', "icons/moon-unselected.svg");
-        darkIcon.setAttribute('title', "Set to dark theme");
-        darkIcon.setAttribute('alt', "Unfilled moon icon");
+        selLightIcon.style.display = 'inline-block';
+        unselLightIcon.style.display = 'none';
+        selDarkIcon.style.display = 'none';
+        unselDarkIcon.style.display = 'inline-block';
     }
 }
 
