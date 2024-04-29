bb363e834587df7989611f16d48462255f361868
b-kucherepa <bkuchr@gmail.com>
2024-04-29 19:50:18 +0400

    refactor: rewrite the code using TS, fix section selection issue

--- src/langselect.ts
+++ src/langselect.ts
@@ -1,10 +1,11 @@
-"use strict";
 loadLang();
-function loadLang() {
-    let lang = localStorage.getItem('lang');
+
+function loadLang(): void {
+    let lang: string | null = localStorage.getItem('lang');
     if (!lang) {
         lang = navigator.language ? navigator.language : "en-EN";
     }
+
     switch (lang.slice(0, 2)) {
         case 'ru':
             window.location.href = "./mainrus.html";
