20710fceb5cecfc1a9eab6d0ea0e4947a4ee12b6
b-kucherepa <bkuchr@gmail.com>
2024-05-22 15:30:18 +0400

    fix: segment type switch statement

--- src/reactComponents/Segment.tsx
+++ src/reactComponents/Segment.tsx
@@ -8,25 +8,32 @@ function Segment(props: any) {
         return <Paragraph data={e} lang={props.lang} />;
       case "infoGrid":
         return <Cell data={e} lang={props.lang} type="general" />;
-      case "resourcesGrid":
+      case "resourceGrid":
         return <Cell data={e} lang={props.lang} type="resource" />;
       default:
-        throw "No such segment type!";
+        throw `No such segment type: ${props.data.type}`;
     }
   });
 
   switch (props.data.type) {
     case "text":
       return <div>{children}</div>;
-    case "infoGrid" || "resourceGrid":
+    case "infoGrid":
       return (
         <>
           <h2>{props.data.header[props.lang]}</h2>
           <div className="grid-container">{children}</div>
         </>
       );
+      case "resourceGrid":
+        return (
+          <>
+            <h2>{props.data.header[props.lang]}</h2>
+            <div className="grid-container">{children}</div>
+          </>
+        );
     default:
-      throw "No such type!";
+      throw `No such segment type: ${props.data.type}`;
   }
 }
 
