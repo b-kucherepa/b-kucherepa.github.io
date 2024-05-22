1650ac58cdced9a52c938595ac7927fdba465e8d
b-kucherepa <bkuchr@gmail.com>
2024-05-22 17:55:38 +0400

    feat: unify the smallest element component, add their different styling support, attune and introduce the respective css classes

--- src/reactComponents/Segment.tsx
+++ src/reactComponents/Segment.tsx
@@ -1,37 +1,64 @@
-import Cell from "./Cell.js";
-import Paragraph from "./Paragraph.js";
+import Element from "./Element.js";
 
 function Segment(props: any) {
   const children: any[] = props.data.elements.map((e: any) => {
+    const classes = { element: "", header: "", text: "" };
     switch (props.data.type) {
       case "text":
-        return <Paragraph data={e} lang={props.lang} />;
-      case "infoGrid":
-        return <Cell data={e} lang={props.lang} type="general" />;
+        classes.text = "indent";
+        break;
+      case "snippetGrid":
+        classes.element = "grid-item";
+        classes.text = "no-indent";
+        break;
+      case "articleGrid":
+        classes.element = "grid-item";
+        classes.text = "indent";
+        break;
       case "resourceGrid":
-        return <Cell data={e} lang={props.lang} type="resource" />;
+        classes.element = "grid-item";
+        classes.header = "author";
+        classes.text = "no-indent";
+        break;
       default:
         throw `No such segment type: ${props.data.type}`;
     }
+
+    return (
+      <Element
+        data={e}
+        lang={props.lang}
+        elementClass={classes.element}
+        headerClass={classes.header}
+        textClass={classes.text}
+      />
+    );
   });
 
   switch (props.data.type) {
     case "text":
       return <div>{children}</div>;
-    case "infoGrid":
+    case "snippetGrid":
       return (
         <>
           <h2>{props.data.header[props.lang]}</h2>
           <div className="grid-container">{children}</div>
         </>
       );
-      case "resourceGrid":
-        return (
-          <>
-            <h2>{props.data.header[props.lang]}</h2>
-            <div className="grid-container">{children}</div>
-          </>
-        );
+    case "articleGrid":
+      return (
+        <>
+          <h2>{props.data.header[props.lang]}</h2>
+          <div className="grid-container">{children}</div>
+        </>
+      );
+    case "resourceGrid":
+      return (
+        <>
+          <h2>{props.data.header[props.lang]}</h2>
+          <div className="grid-container">{children}</div>
+        </>
+      );
     default:
       throw `No such segment type: ${props.data.type}`;
   }
