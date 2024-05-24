9e68a1d92e4b214cfc961d0b54f0f0214f2dc0d4
b-kucherepa <bkuchr@gmail.com>
2024-05-24 20:17:26 +0400

    feat: add new info to the data.json, correct some typos

--- build/htmlBuilder.js
+++ build/htmlBuilder.js
@@ -1,13 +1,16 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-import { renderToString } from 'react-dom/server';
-import { readFile, writeFile } from 'fs/promises';
-import Page from './reactComponents/Page.js';
+import { renderToString } from "react-dom/server";
+import { readFile, writeFile } from "fs/promises";
+import DataPage from "./reactComponents/DataPage.js";
+import IndexPage from "./reactComponents/IndexPage.js";
 const DOCTYPE = "<!DOCTYPE html>";
-const generateFiles = (data) => {
+function generateFiles(data) {
+    writeFile("index.html", DOCTYPE + renderToString(_jsx(IndexPage, { data: data })));
     for (let lang of data.langs) {
-        writeFile(`GENERATED${lang}.html`, DOCTYPE + renderToString(_jsx(Page, { data: data, lang: lang })));
+        writeFile(`${data.pagePrefix}${lang}.html`, DOCTYPE + renderToString(_jsx(DataPage, { data: data, lang: lang })));
     }
-};
+}
 readFile("src/data.json")
-    .then((json) => JSON.parse(json.toString()))
+    .then((buffer) => buffer.toString())
+    .then((json) => JSON.parse(json))
     .then((data) => generateFiles(data));
